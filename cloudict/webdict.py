3825c2799f98cb5feaeed0db4a6a354a9f944732
y² <yy@yuyuan.org>
2018-05-27 20:50:47 -0700

    more unit test

--- cloudict/webdict.py
+++ cloudict/webdict.py
@@ -3,10 +3,8 @@ r"""Mapping Key to WebResource.
 Example::
 
     >>> from pprint import pprint
-    >>> def make_url(make):
-    ...     return 'https://vpic.nhtsa.dot.gov/api/vehicles/getmakeformanufacturer/%s?format=json' % make
-    ...
-    >>> manufacturer_dict = WebDict(make_url, json.loads)
+    >>> api_base = 'https://vpic.nhtsa.dot.gov/api/vehicles'
+    >>> manufacturer_dict = WebDict(lambda s: api_base + '/getmakeformanufacturer/' + s + '?format=json', json.loads)
     >>> pprint(manufacturer_dict['tesla'])
     {'Count': 1,
      'Message': 'Results returned successfully',
@@ -22,16 +20,37 @@ import collections
 import urllib.request
 import urllib.error
 
+from pprint import pformat
+
 
 class WebDictError(Exception):
     pass
 
 
 class UnavailableWebResourceError(WebDictError):
+    """
+    >>> bad_dict = WebDict(lambda s: 'http://example.com/' + s)
+    >>> bad_dict['foo']
+    Traceback (most recent call last):
+        ...
+    UnavailableWebResourceError: failed to get http://example.com/foo: [Errno 8] nodename nor servname provided, or not known
+    """
     pass
 
 
 class InvalidWebResourceError(WebDictError):
+    """
+    >>> wrong_dict = WebDict(lambda s: 'https://vpic.nhtsa.dot.gov/api/vehicles/getmakeformanufacturer/' + s) # will get xml
+    >>> wrong_dict['tesla']
+    Traceback (most recent call last):
+        ...
+    InvalidWebResourceError: failed to parse response from https://vpic.nhtsa.dot.gov/api/vehicles/getmakeformanufacturer/tesla: Expecting value: line 1 column 1 (char 0)
+    ('<Response xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
+     'xmlns:xsd="http://www.w3.org/2001/XMLSchema"><Count>1</Count><Message>Results '
+     'returned '
+     'successfully</Message><SearchCriteria>Manufacturer:tesla</SearchCriteria><Results><MakesForMfg><Mfr_Name>TESLA, '
+     'INC.</Mfr_Name><Make_ID>441</Make_ID><Make_Name>Tesla</Make_Name></MakesForMfg></Results></Response>')
+    """
     pass
 
 
@@ -52,7 +71,8 @@ class WebDict(collections.UserDict):
         try:
             self[key] = json.loads(s)
         except Exception as e:
-            raise UnavailableWebResourceError(str.format('failed to parse response from {}: {}', url, e))
+            raise InvalidWebResourceError(str.format('failed to parse response from {}: {}\n{}', url, e,
+                                                     pformat(s.decode('utf8'))))
 
         return self[key]
 
